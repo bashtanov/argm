17329560c9d9f2955899f88fc4e57ea70f7cf5e9
Alexey Bashtanov <alexey@brandwatch.com>
2018-09-03 10:48:26 +0100

    adapt to pg10

--- argm--1.0.3--1.1.0.sql
+++ argm--1.0.3--1.1.0.sql
@@ -1,3 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION argm UPDATE TO '1.1.0'" to load this file. \quit
-TODO!!!
+do $$ begin
+       raise 'Sorry, argm upgrade was not implemented. Please DROP and CREATE EXTENSION instead';
+end; $$;
